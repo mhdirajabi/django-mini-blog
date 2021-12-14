c527693d926c076dd166feb09e69e93d0b59fdd4
mhdirajabi <mehdirajabi@disroot.org>
2021-12-14 18:09:45 +0330

    User Auth Pt.2

--- Mehdi_Rajabi_Mini_Project_1_Maktab60/blog/members/urls.py
+++ Mehdi_Rajabi_Mini_Project_1_Maktab60/blog/members/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserRegisterationView
 
 urlpatterns = [
-   path('register/', UserRegisterationView.as_view(), name='register')
+   path('register/', UserRegisterationView.as_view(), name='register'),
 ]
